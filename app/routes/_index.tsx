5d9b4cccd48889fe64586a2134d76d93fc8d1732
Volnetiks <38158984+Volnetiks@users.noreply.github.com>
2025-01-15 14:14:11 +0100

    Changes

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -3,10 +3,51 @@ import { Button, Divider, Image, Link } from '@nextui-org/react';
 import React from 'react';
 import WorkingExperience from '~/components/WorkingExperience';
 import Expertise from '~/components/Expertise';
-import { Building2Icon, GithubIcon, LinkedinIcon, SmartphoneIcon } from 'lucide-react';
+import { Building2Icon, GithubIcon, Instagram, LinkedinIcon, SmartphoneIcon } from 'lucide-react';
 import Footer from '~/components/Footer';
+import Markdown from 'react-markdown';
+import remarkMath from 'remark-math';
+import remarkGfm from 'remark-gfm';
+import rehypeKatex from 'rehype-katex';
+import rehypeHighlight from 'rehype-highlight';
+import rehypeRaw from 'rehype-raw';
 
 export default function Index() {
+  const markdownText =
+    `# First Impressions of Warsaw
+
+Warsaw is an incredible mix of old and new. As the capital of Poland, it's full of history, but it also feels super modern with its skyscrapers and tech scene. It's definitely a place that will keep me on my toes, with something always happening.
+
+![Warsaw City](https://i.redd.it/4g32yp3kfug31.jpg)
+
+# Studying at Warsaw University of Technology
+
+I'll be studying Computer Science at Warsaw University of Technology (WUT), one of the best technical universities in Europe. The campus looks impressive, and the vibe feels really international. I'm excited to be part of such a respected school, especially when tech and engineering are my thing.
+
+# Getting There from Angers
+
+To get to Warsaw, I'll fly out of **Nantes (NTE / AF7507)**, with a layover in **Paris (CDG / AF1146)**, and land at Warsaw Chopin Airport (WAW). The flight looks smooth, and from the airport, getting into the city seems pretty easy with public transport or taxis.
+
+![Flight Trip](/app/assets/flight_trip.png)
+
+# My Place in Warsaw
+
+I'll be living in a studio on Braci Załuskich Street, which is in the northern part of the city. It's a quiet, comfortable area with everything I need nearby. Plus, it's well-connected, so getting to WUT or exploring Warsaw won't be a problem.
+
+![Apartment](https://galeria.domiporta.pl/pictures/small/14/2b/f9/f92b4775c571654fa748521979c968891b0e2cf9/wynajme-mieszkanie-warszawa-wilanow.jpg)
+
+# Student Life and ESN
+
+One of the things I'm most looking forward to is student life here. I've heard amazing things about the Erasmus Student Network (ESN). They organize events, trips, and meetups, which will be perfect for making friends and getting to know the city. Their ESNcard sounds like a must—it provides discounts on transport, restaurants, and more.
+
+![ESN](https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSyF0003BP2WBbTBUHwbAMhIAq60SE7Mp5mZA&s)
+
+# Good Deals with the ESNcard
+
+Speaking of discounts, the ESNcard seems like it will be a lifesaver. It offers reduced prices on everything from public transport to museums and even some cafes. It's definitely something I'll get when I arrive.
+
+I'm excited for my upcoming time in Warsaw!`;
+
   return (<div>
     <title>Thomas Béchu</title>
     <Header activatedIndex={0} />
@@ -15,50 +56,21 @@ export default function Index() {
         className={'flex flex-col text-center lg:items-start items-center lg:text-left gap-3 lg:max-w-[35%] max-w-[80%] mb-8'}>
         <Image width={200} src={'/me.jpg'} />
         <p className={'font-bold text-6xl mt-4'}>I'm Thomas Béchu</p>
-        <p className={'text-2xl'}>AI and Mobile programmer based in Nantes, France</p>
+        <p className={'text-2xl'}>A 5th Semester Erasmus Student in Warsaw University of Technology</p>
         <div className={'flex flex-row gap-4'}>
-          <Link href={'https://www.linkedin.com/in/bechu-thomas'} className={'text-black'}>
-            <LinkedinIcon size={30} />
-          </Link>
-          <Link href={'https://github.com/Volnetiks'} className={'text-black'}>
-            <GithubIcon size={30} />
+          <Link href={'https://www.instagram.com/thomas_bechu/'} className={'text-black'}>
+            <Instagram size={30} />
           </Link>
         </div>
       </div>
       <div className={'flex flex-col max-w-[80%] lg:max-w-[35%] gap-4'}>
-        <div className={'flex flex-col gap-8'}>
-          <p className={'text-4xl text-center lg:text-left break-words'}>Passionate crafting products around new
-            technologies.</p>
-          <div className={'flex flex-row gap-4 mb-6 justify-center lg:justify-start'}>
-            <Button className={'bg-black text-white'} href={'/contact'} as={Link}>
-              Contact me
-            </Button>
-            <Button className={'bg-white border-1 border-gray-300 text-black'} href={'/projects'} as={Link}>
-              See my work
-            </Button>
+        <div className={'flex flex-col gap-6 mb-6'}>
+          <div className="prose lg:prose-xl flex-none justify-center min-w-full px-16">
+            <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/katex@0.16.8/dist/katex.min.css" />
+            <Markdown remarkPlugins={[remarkMath, remarkGfm]}
+                      rehypePlugins={[rehypeKatex, [rehypeHighlight, { ignoreMissing: true }], rehypeRaw]}>{markdownText
+            }</Markdown>
           </div>
-        </div>
-        <Divider />
-        <div className={'flex flex-col gap-6 mb-6'}>
-          <p className={'text-large font-medium'}>Areas of expertise</p>
-          <Expertise
-            activityDescription={'Creating Artificial Intelligence tools that matches your needs, for your company. I specialize in chatbot, fitting the model to your needs.'}
-            activityName={'Artificial Intelligence Software'}
-            icon={<Building2Icon size={80} />}
-          />
-          <Expertise
-            activityDescription={'Creating mobile apps for your start-up/company. I create simple, yet good looking and efficient apps to fit your needs.'}
-            activityName={'Mobile apps development'}
-            icon={<SmartphoneIcon size={80} />}
-          />
-        </div>
-        <Divider />
-        <div className={'flex flex-col gap-6 mb-6'}>
-          <p className={'text-large font-medium'}>Working Experience</p>
-          <WorkingExperience companyName={'D-EDGE'} postName={'DevOps Engineer Intern'} duration={'January 2023'}
-                             icon={'/dedge.png'} />
-          <WorkingExperience companyName={'Thomas Béchu\'s Corp.'} postName={'Self Employed Programmer'}
-                             duration={'Sep. 2024 - Present'} icon={''} />
         </div>
       </div>
     </div>
--- app/routes/teamwork.tsx
+++ app/routes/teamwork.tsx
@@ -1,10 +1,90 @@
+import MainArticle from '~/components/MainArticle';
+import Article from '~/components/Article';
 import Header from '~/components/Navbar';
+import { useLoaderData } from '@remix-run/react';
+import { supabase } from '~/database/database';
+import Tag from '~/interfaces/Tag';
+import PostWithTags from '~/interfaces/PostWithTags';
+import React from 'react';
+import { Divider, Link } from '@nextui-org/react';
+import Footer from '~/components/Footer';
+import { MoveRight, Search } from 'lucide-react';
+
+export const loader = async () => {
+
+  const data = await supabase.getRecentPosts(3);
+
+  const posts: PostWithTags[] = [];
+
+  const formatOptions: Intl.DateTimeFormatOptions = {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric'
+  };
+
+  data.forEach((dataItem) => {
+    const tags: Tag[] = [];
+
+    dataItem['tags'].forEach((tagData: any) => {
+      const tag: Tag = {
+        id: tagData.id,
+        name: tagData.name,
+        icon: tagData.icon,
+        color: tagData.color
+      };
+
+      tags.push(tag);
+    });
+
+    const date = new Date(dataItem['created_at']);
+
+    const formattedDate = date.toLocaleString('en-US', formatOptions);
+
+    const post: PostWithTags = {
+      title: dataItem['title'],
+      description: dataItem['description'],
+      content: dataItem['content'],
+      created_at: formattedDate,
+      image: dataItem['image'],
+      tags: tags,
+      id: dataItem['id']
+    };
+
+    posts.push(post);
+  });
+
+  return posts;
+};
 
 export default function Teamwork() {
-  return (<div className={'h-screen'}>
-    <Header activatedIndex={4} />
-    <div className={'h-full flex justify-center items-center'}>
-      Coming soon...
+  const posts = useLoaderData<typeof loader>();
+
+  return (
+    <div>
+      <title>Thomas Béchu | Blog</title>
+      <Header activatedIndex={1} />
+      <main>
+        <div className={'flex flex-col pt-6'}>
+          <div className={'flex flex-row justify-end mb-2 mr-20'}>
+            <Link href={'/search'} className={'font-bold text-black'}>
+              <p className={'mr-2'}>Search</p>
+              <Search />
+            </Link>
+          </div>
+          <div className={'flex-col lg:flex-row hidden lg:flex lg:justify-around'}>
+            <Article post={posts[0]} />
+            <Article post={posts[1]} />
+            <Article post={posts[2]} />
+          </div>
+          <div className={'flex flex-col lg:hidden items-center gap-y-16'}>
+            <Article post={posts[0]} />
+            <Article post={posts[1]} />
+            <Article post={posts[2]} />
+          </div>
+        </div>
+      </main>
+      <Divider />
+      <Footer />
     </div>
-  </div>);
+  );
 }
